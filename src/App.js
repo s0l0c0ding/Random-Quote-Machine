73a0202d91ac8e32271342386565e8759af7b4b4
s0l0c0ding <s0l0c0ding@github.com>
2020-11-04 18:36:07 +0100

    :art: add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Qutoe from './components/Quote'
 import './App.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 function App() {
   return (
-    <div className="App">
-      <Qutoe />
+    <div className=" d-flex align-items-center min-vh-100">
+    <Container>
+      <Row className="justify-content-center align-middle">
+          <Qutoe />
+      </Row>
+    </Container>
     </div>
   );
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,28 +1,60 @@
 import { connect } from "react-redux";
 import { getNewQuote } from "../redux/actions";
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Twi from '../assets/twIcon.svg'
 
 class Qutoe extends React.Component {
 
     render() {
-        const tweet = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='${this.props.quote}' '${this.props.author}'`
         const quotes = this.props.quotes;
-        const id =  this.props.id 
+        const id = this.props.id;
+        const tweet = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='${quotes[id].quote} ${quotes[id].author}'`
+        const sty = ['primary','secondary','success','danger','warning','info','dark','light'][id>8 ? 8: id];
         return (
-        <div id='quote-box'>
-        <h1 id='text'>{quotes[id].quote}</h1>
-        <h1 id='author' >{quotes[id].author}</h1>
-        <button id='new-quote' onClick={() => this.props.getNewQuote()} >getNewOne</button>
-        <a id='tweet-quote' href= {tweet} target='_blank'  rel='noreferrer'>tweet it</a>
-        </div>
+            <Card border = {sty}>
+                <div id='quote-box'>
+                    <Card.Body>
+                        <blockquote className="blockquote mb-0">
+                            <p id='text'>{quotes[id].quote}</p>
+                            <Container>
+                                <Row>
+                                    <Col xs lg="8">
+                                    <OverlayTrigger  placement="bottom" overlay={<Tooltip id="tooltip-bottom">Get a new quote!</Tooltip>}>
+                                        <Button id='new-quote' className="mr-2" variant={sty} size="sm" onClick={() => this.props.getNewQuote()} >NewQuote</Button>
+                                        </OverlayTrigger>
+                                        <OverlayTrigger  placement="bottom" overlay={<Tooltip id="tooltip-bottom">Tweet it</Tooltip>}>
+                                        <a id='tweet-quote' href={tweet} target='_blank' rel='noreferrer' alt='tweet it'>
+                                        <img src={Twi} alt='twitter logo' style= {{ maxHeight:'26px', maxWidth:'26px'}}/>
+                                        </a>
+                                        </OverlayTrigger>
+                                    </Col>
+                                    <Col xs lg="4">
+                                        <footer className="blockquote-footer">
+                                            <cite id='author' className="">{quotes[id].author}</cite>
+                                        </footer>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </blockquote>
+                    </Card.Body>
+                </div>
+            </Card>
         )
     }
 }
+
 
 const mapStateToProps = (state) => {
     return state;
 }
 
-const mapDispatchToProps =  {getNewQuote};
+const mapDispatchToProps = { getNewQuote };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Qutoe)
+export default connect(mapStateToProps, mapDispatchToProps)(Qutoe)  
